983d7beef0b5d6600188929f16173207c808d2fa
Carlos12001 <carlos.andres12001@gmail.com>
2023-05-06 16:36:39 -0600

    feat: sommithings work

---
+++ newton_raphson_system.m
@@ -0,0 +1,44 @@
+function [x_k, k, e_k, errors] = newton_raphson_system(x0, f, x, tol, iterMax)
+  pkg load symbolic;
+  % Crear símbolos para las funciones y las variables
+  f_sym = sym(zeros(1, length(f)));
+  x_sym = sym(zeros(1, length(x)));
+  for i = 1:length(f)
+    f_sym(i) = sym(f{i});
+    x_sym(i) = sym(x{i});
+  end
+  
+  % Calcular el Jacobiano
+  J = jacobian(f_sym, x_sym);
+  
+  % Definir las funciones a utilizar en Octave
+  f_func = @(input) double(subs(f_sym, x_sym, input));
+  J_func = @(input) double(subs(J, x_sym, input));
+
+  
+  % Inicializar variables
+  x_k = x0;
+  e_k = inf;
+  k = 0;
+  errors = [];
+  
+  % Bucle del método de Newton-Raphson
+  while e_k > tol && k < iterMax
+    F = f_func(x_k);
+    J_inv = inv(J_func(x_k));
+    
+    x_k1 = x_k - J_inv * F;
+    e_k = norm(F, 2);
+    
+    errors = [errors; e_k];
+    x_k = x_k1;
+    k = k + 1;
+  end
+  
+  % Crear gráfica de iteraciones vs errores
+  figure;
+  plot(1:k, errors, '-o');
+  xlabel('Iteraciones');
+  ylabel('Error');
+  title('Newton-Raphson: Iteraciones vs Error');
+end
